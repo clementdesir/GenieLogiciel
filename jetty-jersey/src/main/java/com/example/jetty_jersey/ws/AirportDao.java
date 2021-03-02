63f0ad4cb63340fb035762166cb59e090e99c6d7
LAPTOP-93CM60UM\victo <victorconotte@live.fr>
2021-03-02 18:14:16 +0100

    test 2

--- jetty-jersey/src/main/java/com/example/jetty_jersey/ws/AirportDao.java
+++ jetty-jersey/src/main/java/com/example/jetty_jersey/ws/AirportDao.java
@@ -1,8 +1,8 @@
 package com.example.jetty_jersey.ws;
 
 public class AirportDao {
-	public List<Airport> searchAirportName(String name);
-	public List<Airport> searchAirportCode(int code);
-	public List<Airport> searchAirportCity(String city);
+	//public List<Airport> searchAirportName(String name);
+	//public List<Airport> searchAirportCode(int code);
+	//public List<Airport> searchAirportCity(String city);
 
 }
